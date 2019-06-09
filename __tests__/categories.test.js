795f37b102771d0018cabd60cede680577e9adff
Matt Wilkin <mwilkin22@gmail.com>
2019-06-09 13:03:14 -0700

    update package.json, write first test for categories, failing.

--- __tests__/categories.test.js
+++ __tests__/categories.test.js
@@ -3,16 +3,30 @@
 const Categories = require('../src/models/categories.js');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const supergoose = require('./supergoose.js');
 
-describe('Categories Module', () => {
+beforeAll(supergoose.startDB);
+afterAll(supergoose.stopDB);
+
+describe('Categories Model', () => {
   let categories;
   beforeEach(() => {
     categories = new Categories;
-
+  });
     describe('constructor', () => {
       it('Can successfully instantiate an empty category', () => {
         expect(categories).toBeDefined();
       });
     });
+  it('can post() a new category', () => {
+    let object = {name: 'Car'};
+    return categories.post(object)
+      .then(record => {
+        Object.keys(object).forEach(key => {
+          expet(record[key]).toEqual(object[key]);
+        });
+      });
+
   });
+  
 });
